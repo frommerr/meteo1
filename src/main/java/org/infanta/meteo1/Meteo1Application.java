d109ea2264822bcfabc6e4e0e0b3c0dac5d69dd6
ilyan1 <shuba110604@gmail.com>
2025-04-04 18:23:26 +0200

    BD migrada a Aiven

--- src/main/java/org/infanta/meteo1/Meteo1Application.java
+++ src/main/java/org/infanta/meteo1/Meteo1Application.java
@@ -2,11 +2,8 @@ package org.infanta.meteo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.infanta.meteo1.controller.WeatherConfig;
 
 @SpringBootApplication
-@EnableConfigurationProperties(WeatherConfig.class)
 public class Meteo1Application {
 
     public static void main(String[] args) {
