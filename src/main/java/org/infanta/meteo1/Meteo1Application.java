3c80aff3fc0f06a533a1f4acb312b5851a35dcac
ilyan1 <shuba110604@gmail.com>
2025-04-04 18:18:47 +0200

    BD migrada a Aiven

--- src/main/java/org/infanta/meteo1/Meteo1Application.java
+++ src/main/java/org/infanta/meteo1/Meteo1Application.java
@@ -12,7 +12,6 @@ public class Meteo1Application {
     public static void main(String[] args) {
          //prueba para el commit
         SpringApplication.run(Meteo1Application.class, args);
-
     }
 
 }
